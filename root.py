ab7860e16fcd3da4672345e63d9a47a57dd0e0a6
Benjamin Bannier <bbannier@gmail.com>
2011-08-29 02:24:52 -0400

    Make matplotlib available in interpreted scripts

--- root.py
+++ root.py
@@ -3,27 +3,6 @@ import ROOT as r
 import sys
 import IPython.ipapi
 
-_ip = IPython.ipapi.get()
-
-r.gROOT.ProcessLine('.x ~/.root_logon.C')
-
-_f = []
-args = sys.argv[1:]
-for a in args:
-  if a.endswith('.C'):
-    print "Processing %s" %(a)
-    r.gROOT.ProcessLine('.x %s' %(a))
-  elif a.endswith('.py'):
-    print "Processing %s" %(a)
-    _ip.magic('run -i %s' %(a))
-  elif a.endswith('.root'):
-    print "Loading %s as _f[%s]" %(a, len(_f))
-    _f.append(r.TFile(a))
-  elif a.endswith('.hbk'):
-    print "Loading %s as _f[%s]" %(a, len(_f))
-    _f.append(r.THbookFile(a))
-
-##################################################
 try:
   from matplotlib import pyplot
   import numpy as np
@@ -72,5 +51,26 @@ try:
 
 except ImportError:
   pass
+##################################################
+
+_ip = IPython.ipapi.get()
+
+r.gROOT.ProcessLine('.x ~/.root_logon.C')
+
+_f = []
+args = sys.argv[1:]
+for a in args:
+  if a.endswith('.C'):
+    print "Processing %s" %(a)
+    r.gROOT.ProcessLine('.x %s' %(a))
+  elif a.endswith('.py'):
+    print "Processing %s" %(a)
+    _ip.magic('run -i %s' %(a))
+  elif a.endswith('.root'):
+    print "Loading %s as _f[%s]" %(a, len(_f))
+    _f.append(r.TFile(a))
+  elif a.endswith('.hbk'):
+    print "Loading %s as _f[%s]" %(a, len(_f))
+    _f.append(r.THbookFile(a))
 
 # vi:filetype=python
