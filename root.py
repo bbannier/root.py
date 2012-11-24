809a7cd570f4cbe1e1a868b1348894f76bb57e2d
Benjamin Bannier <bbannier@bnl.gov>
2012-11-24 08:07:59 -0500

    Add a proper license statement

--- root.py
+++ root.py
@@ -1,4 +1,14 @@
 #!/bin/env ipython
+
+"""
+Copyright 2010-2012 Benjamin Bannier
+
+This program is free software. It comes without any warranty, to the extent
+permitted by applicable law. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2, as
+published by Sam Hocevar. See http://sam.zoy.org/wtfl/COPYING for more details.
+"""
+
 import ROOT as r
 import sys
 import IPython.ipapi
